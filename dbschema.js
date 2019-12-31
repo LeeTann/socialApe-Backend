95baa8210fdbd08ecf5f4f2b4866fbd6523352de
ltann <leeroyleeroy18@gmail.com>
2019-12-31 12:54:45 -0800

    Installed express and reformat GET and Post

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createAt: '2019-12-31T20:45:51.948Z',
+            likeCount: 5, 
+            commentCount: 2
+        }
+    ]
+}
