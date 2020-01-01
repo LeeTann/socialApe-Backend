dcfe4a9323d25e408c3416acb3ae12781582c3a3
ltann <leeroyleeroy18@gmail.com>
2020-01-01 15:58:23 -0800

    Refactor code to be modular

---
+++ functions/utils/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCT5fIl1KoO9YRrMGW5O8kFxSlN20GDI1g",
+    authDomain: "socialape-3e674.firebaseapp.com",
+    databaseURL: "https://socialape-3e674.firebaseio.com",
+    projectId: "socialape-3e674",
+    storageBucket: "socialape-3e674.appspot.com",
+    messagingSenderId: "19995462488",
+    appId: "1:19995462488:web:789822fb995ecf3ede732d",
+    measurementId: "G-YRPS21S0V9"
+}
